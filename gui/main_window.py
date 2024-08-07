4745d67a9b1c1928ca0872a1c26615cd4adec0d5
DownMyStuff <nmcr.eve@gmail.com>
2024-08-07 13:17:15 +0300

    Added disable buttons if no_license condition

--- gui/main_window.py
+++ gui/main_window.py
@@ -50,24 +50,54 @@ class MainWindow(QMainWindow):
 
         icon_path = os.path.join(os.path.dirname(__file__), '..', 'ico')
 
-        add_order_action = QAction(QIcon(os.path.join(icon_path, 'Nova.png')), "Додати замовлення", self)
-        add_order_action.setStatusTip("Додати нове замовлення")
-        add_order_action.triggered.connect(self.add_order)
-        toolbar.addAction(add_order_action)
+        self.add_order_action = QAction(QIcon(os.path.join(icon_path, 'Nova.png')), "Додати замовлення", self)
+        self.add_order_action.setStatusTip("Додати нове замовлення")
+        self.add_order_action.triggered.connect(self.add_order)
+        toolbar.addAction(self.add_order_action)
 
-        update_status_action = QAction(QIcon(os.path.join(icon_path, 'refresh.png')), "Оновити статус", self)
-        update_status_action.setStatusTip("Оновити статус замовлення")
-        update_status_action.triggered.connect(self.update_status)
-        toolbar.addAction(update_status_action)
+        self.update_status_action = QAction(QIcon(os.path.join(icon_path, 'refresh.png')), "Оновити статус", self)
+        self.update_status_action.setStatusTip("Оновити статус замовлення")
+        self.update_status_action.triggered.connect(self.update_status)
+        toolbar.addAction(self.update_status_action)
+
+        # Нові кнопки
+        self.contacts_action = QAction(QIcon(os.path.join(icon_path, 'contact.png')), "Контакти", self)
+        self.contacts_action.setStatusTip("Переглянути контакти")
+        self.contacts_action.triggered.connect(self.view_contacts)
+        toolbar.addAction(self.contacts_action)
+
+        self.products_action = QAction(QIcon(os.path.join(icon_path, 'boxes.png')), "Товари", self)
+        self.products_action.setStatusTip("Переглянути товари")
+        self.products_action.triggered.connect(self.view_products)
+        toolbar.addAction(self.products_action)
+
+        self.reports_action = QAction(QIcon(os.path.join(icon_path, 'report.png')), "Звіти", self)
+        self.reports_action.setStatusTip("Переглянути звіти")
+        self.reports_action.triggered.connect(self.view_reports)
+        toolbar.addAction(self.reports_action)
+
+        self.settings_action = QAction(QIcon(os.path.join(icon_path, 'settings.png')), "Налаштування", self)
+        self.settings_action.setStatusTip("Відкрити налаштування")
+        self.settings_action.triggered.connect(self.open_settings)
+        toolbar.addAction(self.settings_action)
 
         spacer_widget = QWidget()
         spacer_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
         toolbar.addWidget(spacer_widget)
 
-        update_key_action = QAction(QIcon(os.path.join(icon_path, 'key.webp')), "Оновити ключ", self)
-        update_key_action.setStatusTip("Оновити ключ")
-        update_key_action.triggered.connect(self.update_key)
-        toolbar.addAction(update_key_action)
+        self.update_key_action = QAction(QIcon(os.path.join(icon_path, 'key.webp')), "Оновити ключ", self)
+        self.update_key_action.setStatusTip("Оновити ключ")
+        self.update_key_action.triggered.connect(self.update_key)
+        toolbar.addAction(self.update_key_action)
+
+        # Розпочати з блокування кнопок, якщо ліцензія не перевірена
+        self.set_buttons_enabled(False)
+
+    def set_buttons_enabled(self, enabled):
+        """Блокує або розблокує всі кнопки на панелі інструментів, окрім кнопки 'Оновити ключ'."""
+        for action in self.findChildren(QAction):
+            if action.text() != "Оновити ключ":
+                action.setEnabled(enabled)
 
     def add_order(self):
         print("Додати замовлення")
@@ -80,8 +110,24 @@ class MainWindow(QMainWindow):
         dialog.exec_()
         self.update_license_info()
 
+    def view_contacts(self):
+        print("Переглянути контакти")
+        # Додайте код для відображення контактів
+
+    def view_products(self):
+        print("Переглянути товари")
+        # Додайте код для відображення товарів
+
+    def view_reports(self):
+        print("Переглянути звіти")
+        # Додайте код для відображення звітів
+
+    def open_settings(self):
+        print("Відкрити налаштування")
+        # Додайте код для відкриття налаштувань
+
     def update_license_info(self):
-        """Оновлює інформацію про строк дії ліцензії."""
+        """Оновлює інформацію про строк дії ліцензії та блокує або розблокує кнопки."""
         device_id = get_device_id()
         license_info = get_license(device_id)
 
@@ -98,12 +144,16 @@ class MainWindow(QMainWindow):
                         color = 'yellow'
                     self.license_info_label.setText(
                         f'<span style="color:{color}">Строк дії ліцензії: до {expiration_date.strftime("%Y-%m-%d")}</span>')
+                    self.set_buttons_enabled(True)
                 else:
                     self.license_info_label.setText('<span style="color:red">Строк дії ліцензії закінчився</span>')
+                    self.set_buttons_enabled(False)
             except ValueError:
                 self.license_info_label.setText('<span style="color:red">Помилка формату дати ліцензії</span>')
+                self.set_buttons_enabled(False)
         else:
             self.license_info_label.setText('<span style="color:red">Ліцензія не знайдена</span>')
+            self.set_buttons_enabled(False)
 
     def center(self):
         # Отримати розміри екрану
